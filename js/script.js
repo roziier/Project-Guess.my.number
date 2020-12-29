73250762b901cbebcaee03211a9435abed96cf2c
roziier <rikirosa93@gmail.com>
2020-12-29 12:39:52 +0100

    js guide line

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// 1-When someone clicks button again all the parameters will be reset instead of highscore
+// 2-The cumputer will generate a secret number
+// 3-The user will click the check button
+// if the user number will be higher than the pc number we'll have:
+// - a message : too high!
+// - a score : -1
+// if the user number will be lower than the pc number we'll have:
+// - a message : too low!
+// - a score : -1
+// if the user number will be correct we'll have:
+// - a message : congratulations!!!
+// - a score : -1 and the actual score
+// highscore: actual score.. if during a game, the highscore will be higher than the previous highscore the new highscore will be replece
+// highscore will be reset just if the user will refresh the page
